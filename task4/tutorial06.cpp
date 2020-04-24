c14456c71bc177fa7edb192227c710c807b738d0
kailiakeugene@mail.ru <vasya@pupkin.ru>
2020-04-24 23:41:59 +0300

    add changes

--- task4/tutorial06.cpp
+++ task4/tutorial06.cpp
@@ -16,7 +16,131 @@ using namespace glm;
 
 #include <common/shader.hpp>
 #include <common/texture.hpp>
-#include <common/controls.hpp>
+
+class Player {
+
+
+	// Initial position : on +Z
+	glm::vec3 position;
+	// Initial horizontal angle : toward -Z
+	float horizontalAngle;
+	// Initial vertical angle : none
+	float verticalAngle;
+	// Initial Field of View
+	float initialFoV;
+
+	float speed;
+	float mouseSpeed;
+
+	glm::mat4 ViewMatrix;
+	glm::mat4 ProjectionMatrix;
+	
+
+public:
+	Player() : position(glm::vec3(0, 0, 5)), horizontalAngle(3.14f), verticalAngle(0.0f), initialFoV(45.0f), speed(3.0f), mouseSpeed(0.001f) {}
+
+	glm::mat4 getViewMatrix() const {
+		return ViewMatrix;
+	}
+	glm::mat4 getProjectionMatrix() const {
+		return ProjectionMatrix;
+	}
+
+	void computeMatricesFromInputsSizes(int window_width, int window_height) {
+
+		// glfwGetTime is called only once, the first time this function is called
+		static double lastTime = glfwGetTime();
+
+		// Compute time difference between current and last frame
+		double currentTime = glfwGetTime();
+		float deltaTime = float(currentTime - lastTime);
+
+		// Get mouse position
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+
+		// Reset mouse position for next frame
+		glfwSetCursorPos(window, window_width / 2, window_height / 2);
+
+		// Compute new orientation
+		horizontalAngle += mouseSpeed * float(window_width / 2 - xpos);
+		verticalAngle += mouseSpeed * float(window_height / 2 - ypos);
+
+		// Direction : Spherical coordinates to Cartesian coordinates conversion
+		glm::vec3 direction(
+			cos(verticalAngle) * sin(horizontalAngle),
+			sin(verticalAngle),
+			cos(verticalAngle) * cos(horizontalAngle)
+		);
+
+		// Right vector
+		glm::vec3 right = glm::vec3(
+			sin(horizontalAngle - 3.14f / 2.0f),
+			0,
+			cos(horizontalAngle - 3.14f / 2.0f)
+		);
+
+		// Up vector
+		glm::vec3 up = glm::cross(right, direction);
+
+		// Move forward
+		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+			position += direction * deltaTime * speed;
+		}
+		// Move backward
+		if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+			position -= direction * deltaTime * speed;
+		}
+		// Strafe right
+		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+			position += right * deltaTime * speed;
+		}
+		// Strafe left
+		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+			position -= right * deltaTime * speed;
+		}
+		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+			position += up * deltaTime * speed;
+		}
+		if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
+			position -= up * deltaTime * speed;
+		}
+
+		float FoV = initialFoV;// - 5 * glfwGetMouseWheel(); // Now GLFW 3 requires setting up a callback for this. It's a bit too complicated for this beginner's tutorial, so it's disabled instead.
+
+		// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+		ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 100.0f);
+		// Camera matrix
+		ViewMatrix = glm::lookAt(
+			position,           // Camera is here
+			position + direction, // and looks here : at the same position, plus "direction"
+			up                  // Head is up (set to 0,-1,0 to look upside-down)
+		);
+
+		// For the next frame, the "last time" will be "now"
+		lastTime = currentTime;
+	}
+
+
+	void computeMatricesFromInputsWindow() {
+		int window_width, window_height;
+		glfwGetWindowSize(window, &window_width, &window_height);
+		computeMatricesFromInputsSizes(window_width, window_height);
+	}
+
+	glm::mat4 getMVP() {
+		// Compute the MVP matrix from keyboard and mouse input
+		computeMatricesFromInputsWindow();
+		glm::mat4 ProjectionMatrix = getProjectionMatrix();
+		glm::mat4 ViewMatrix = getViewMatrix();
+		glm::mat4 ModelMatrix = glm::mat4(1.0);
+		return ProjectionMatrix * ViewMatrix * ModelMatrix;
+	}
+};
+
+
+Player player;
+
 
 int main( void )
 {
@@ -190,11 +314,7 @@ int main( void )
 		glUseProgram(programID);
 
 		// Compute the MVP matrix from keyboard and mouse input
-		computeMatricesFromInputs();
-		glm::mat4 ProjectionMatrix = getProjectionMatrix();
-		glm::mat4 ViewMatrix = getViewMatrix();
-		glm::mat4 ModelMatrix = glm::mat4(1.0);
-		glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+		glm::mat4 MVP = player.getMVP();
 
 		// Send our transformation to the currently bound shader, 
 		// in the "MVP" uniform
