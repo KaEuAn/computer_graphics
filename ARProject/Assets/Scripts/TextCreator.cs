0e2605a5aef54f5f1987a90036ebe473bd073798
kailiakeugene@mail.ru <vasya@pupkin.ru>
2020-05-30 01:10:05 +0300

    complete tiger

---
+++ ARProject/Assets/Scripts/TextCreator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextCreator : MonoBehaviour
+{
+
+    public string text = "xxxVanya petuhxxx";
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<TextMesh>().text = text;
+        }
+    }
+}
