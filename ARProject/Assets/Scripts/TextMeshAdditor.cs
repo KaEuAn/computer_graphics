f5ba8b0a40dc5c92b92a8dff6ae7074fa27f0a0b
kailiakeugene@mail.ru <vasya@pupkin.ru>
2020-05-29 21:59:17 +0300

    add textmesh

---
+++ ARProject/Assets/Scripts/TextMeshAdditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextMeshAdditor : MonoBehaviour
+{
+    public Material mat;
+    public Font font;
+
+    void Start()
+    {
+
+
+        // create 3d text mesh
+        GameObject Text = new GameObject();
+
+        TextMesh textMesh = (TextMesh)GetComponent(typeof(TextMesh));
+        Text.AddComponent(textMesh);
+        textMesh.font = font;
+        MeshRenderer meshRenderer = gameObject.AddComponent(MeshRenderer);
+        meshRenderer.material = mat;
+
+        textMesh.text = "Hello World!";
+
+    }
+}
