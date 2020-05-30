e752ce58b63101c0b6dbb330f85c0495f12d9261
kailiakeugene@mail.ru <vasya@pupkin.ru>
2020-05-30 18:11:28 +0300

    add download from google drive

--- ARProject/Assets/Scripts/GoogleApi.cs
+++ ARProject/Assets/Scripts/GoogleApi.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityGoogleDrive;
 using UnityGoogleDrive.Data;
+using Newtonsoft.Json;
 
 public class GoogleApi : MonoBehaviour
 {
-    private List<File> files;
-    public string filename = "";
+
+
+    private List<UnityGoogleDrive.Data.File> files;
+    public UnityGoogleDrive.Data.File keyfile = null;
+    public string json;
+    public Dictionary<string, string> text_for_walls;
+
 
     void Start()
     {
@@ -15,9 +22,10 @@ public class GoogleApi : MonoBehaviour
 
     void Update()
     {
-        if (!(files is null) && files.Count != 0 && filename.Length == 0)
+        if (!(files is null) && files.Count != 0 && keyfile is null)
         {
-            filename = files[0].Name;
+            keyfile = files[0];
+            GoogleDriveFiles.Download(keyfile.Id).Send().OnDone += file => text_for_walls = JsonConvert.DeserializeObject<Dictionary<string, string>>(Encoding.UTF8.GetString(file.Content));
         }
     }
 }
